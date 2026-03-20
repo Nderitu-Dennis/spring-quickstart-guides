871631b3d59d2004ce1a24ac88907c69c4ab2344
Nderitu-Dennis <nderitudenis87@gmail.com>
2026-03-20 13:19:55 +0300

    demo 1 from spring.io

---
+++ demo/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication /*this is a meta annotation cz it combines
+ @SpringBootConfiguration,
+@EnableAutoConfiguration,
+ @ComponentScan
+*/
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+    @GetMapping("/hello") //use this method to answer requests from /hello endpoint
+    public String hello (@RequestParam(value="name", defaultValue= "World") String name){
+        return String.format("Hello %s!",name);
+        // check this string format
+    }
+
+}
