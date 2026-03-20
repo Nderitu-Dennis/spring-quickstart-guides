4ce3af343d5ad377e753c1a792d16f64cbf180f3
Nderitu-Dennis <nderitudenis87@gmail.com>
2026-03-20 15:45:52 +0300

    develop a RESTful web service with Spring, Records in Java

---
+++ README.md
@@ -0,0 +1,1 @@
+* Read abt `Records` [here](https://github.com/Nderitu-Dennis/real-nigga-notes-the-benjamins/blob/main/backend_mtcos_lessons.md#java-record)
---
+++ rest-service/src/main/java/com/example/rest_service/Greeting.java
@@ -0,0 +1,5 @@
+package com.example.rest_service;
+
+// a Record is a short, immutable way to define a class.
+public record Greeting(long id, String content) {
+}
